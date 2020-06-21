a31f25b306b38957e4b84aea76899a601baae6ea
EdwinOlmedo <edwin.j187.@hotmail.com>
2020-06-21 17:11:59 -0600

    Entrar

---
+++ views/login.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+    <link rel="stylesheet" href="../css/styles.css"/>
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@700&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Comfortaa:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<br>
+<br>
+<br>
+<br>
+    <section>
+    <article>
+    <div id="form">
+    <form action="" class="form-horizontal" name="formulario" id="salario">
+    <div>
+    <h2>Registro</h2>
+    <hr>
+    </div>
+    
+    <div class="input-container">
+    <i class="fa fa-user-circle-o icon"></i>
+    <input class="input-field" type="text" name="input" placeholder="Usuario:">
+    </div>
+
+    <div class="input-container">
+    <i class="fa fa-address-card icon"></i>
+    <input class="input-field" type="text" name="input" placeholder="Nombre:">
+    </div>
+
+    <div class="input-container">
+    <i class="fa fa-address-card icon"></i>
+    <input class="input-field" type="text" name="input" placeholder="Apellido:">
+    </div>
+
+    <div class="input-container">
+    <i class="fa fa-birthday-cake icon"></i>
+    <input class="input-field" type="text" name="fechacumpleaños" placeholder="dd/mm/yyyy">
+    </div>
+
+    <div class="input-container">
+    <i class="fa fa-envelope icon"></i>
+    <input class="input-field" type="text" name="email" placeholder="Email:">
+    </div>
+
+    <div class="input-container">
+    <i class="fa fa-phone icon"></i>
+    <input class="input-field" type="text" name="tel" placeholder="Numero de teléfono:">
+    </div>
+
+    <div class="input-container">
+    <i class="fa fa-key icon"></i>
+    <input class="input-field" type="password" name="pass" placeholder="Ingrese su contraseña:">
+    </div>
+
+    <div class="input-container">
+    <i class="fa fa-key icon"></i>
+    <input class="input-field" type="password" name="pass_rep" placeholder="Repita su contraseña:">
+    </div>
+    <button type="submit" class="btn">Registrar</button>
+    <h4>¿Ya tienes cuenta?</h4>
+    <center><a href="">Inicia Sesión</a></center>
+    </form>
+    </div>
+    </article>
+    </section>
+    
+</body>
+</html>
