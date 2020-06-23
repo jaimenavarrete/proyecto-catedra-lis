2aad5695d591ce40382a32cc04b8daeb7c4b23c3
EdwinOlmedo <edwin.j187.@hotmail.com>
2020-06-23 17:15:07 -0600

    Agrego select al diseño para los grupos de laboratorio en la inscripcion de materias

--- views/inscripcon_materias.php
+++ views/inscripcon_materias.php
@@ -35,28 +35,32 @@
     <br>
      <br>
     <div class="input-container-repo">
-    <h4 style="margin-top:-0.1em;" >Materia:</h4>
+    <h4 style="margin-top:-0.1em; padding:5px;" >Materia:</h4>
     <select name="materias" id="materias">
     <option value="">Matematica 1</option>
     <option value="">Matematica 2</option>
     <option value="">Matematica 3</option>
     </select>
-    </div>
-    
-    <div class="input-container-repo">
-    <h4 style="margin-top:-0.1em;">Grupo:</h4>
-    <select name="grupo" id="grupo">
+    <h4 style="margin-top:-0.1em;" class="select_grupos_nombre">Grupo teoria:</h4>
+    <select name="grupo" id="grupo" class="select_grupos_teoria">
     <option value="">01T</option>
     <option value="">04T</option>
     <option value="">02T</option>
     </select>
     </div>
-
+    
     <div class="input-container-repo">
-    <h4 style="margin-top:-0.1em;">Cupo:</h4>
-        <input type="text" name="cupos" id="cupos">
+    <h4 style="margin-top:-0.1em; margin-left: 5.5px; ">Cupo:</h4>
+        <input type="text" name="cupos" id="cupos" style="margin-left: 1.5em; width: 140px;" >
+    
+    <h4 style="margin-top:-0.1em;" class="select_grupos_lab">Grupo laboratorio:</h4>
+    <select name="grupo" id="grupo" class="select_grupos_lab">
+    <option value="">01L</option>
+    <option value="">04L</option>
+    <option value="">02L</option>
+    </select>
     </div>
-    <button type="submit" class="btn" id="btn-repo">+ Agregar materia</button>
+    <button type="submit" class="btn" id="btn-repo" >+ Agregar materia</button>
     </div>
     </article>
     </section>
