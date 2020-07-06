b76288c600571a586347d6387be46ee3c8382e4d
Diego <diegofytn@gmail.com>
2020-07-06 11:33:43 -0600

    asignación de grupos en la bd

--- queries/create_groups.php
+++ queries/create_groups.php
@@ -25,6 +25,7 @@ function create_groups($total_students, $amount){
     $students_out = $total_students - ($total_groups*$amount);
     return array($total_groups, $students_out);
 }
+//Función que genera indices aleatorios no repetidos
 function get_idx($total,$added){
     $id = rand(0, $total-1);
     if(in_array($id, $added)){
@@ -33,49 +34,92 @@ function get_idx($total,$added){
         return $id;
     }
 }
-if(isset($_GET['create_groups'])){
+//Función para formar el nombre del grupo
+function get_name($subject1, $subject2, $connection){
+    $stmt = "CALL count_groups('$subject1', '$subject2')";
+    $result = mysqli_query($connection, $stmt);
+    if(!$result){
+        echo "Error en la ejecución.";
+        return;
+    }
+    $number = mysqli_fetch_array($result);
+    $name = substr($subject1, 0, 3) . substr($subject2, 0, 3);
+    $name = $name . ($number[0]+1);
+    mysqli_next_result($connection);
+    return $name;
+}
+function insert_group($connection, $group_name, $employee, $sbjt1, $sbjt2){
+    $stmt = "CALL create_groups('$group_name', '$sbjt1', '$sbjt2', '$employee')";
+    $result = mysqli_query($connection, $stmt);
+    if($result){
+        mysqli_next_result($connection);
+        echo "Grupo creado";    
+    }else{
+        echo mysqli_error($connection). $stmt;
+    }
+}
+//Función que asigna los estudiantes a cada grupo
+function add_student($connection, $student, $group){
+    $stmt = "CALL assign_group('$student', '$group')";
+    $result = mysqli_query($connection, $stmt);
+    if($result){
+        mysqli_next_result($connection);
+        echo "Grupo asignado";    
+    }else{
+        echo mysqli_error($connection). $stmt;
+    }
+}
+//Si le han hecho click al botón y la cantidad no es cero
+if(isset($_GET['create_groups']) && isset($_GET["cupos"])){
     $s1 = $_GET['materia1'];
     $s2 = $_GET['materia2'];
     $n = $_GET["cupos"];
-    
+    //Listas de alumnos inscritos
     $students_s1 = count_students($con, $s1);
     $students_s2 = count_students($con, $s2);    
+    //Cantidad de alumnos en cada materia
     $students_m1 = count($students_s1);
     $students_m2 = count($students_s2);
-
-
+    //Array de estudiantes que quedan sin grupo
+    $out = [];
+    //Hacer los arrays del mismo tamaño
     if ($students_m1 > $students_m2) {
         $substraction = $students_m1 - $students_m2;
         for ($i=0; $i < $substraction; $i++) { 
-            array_pop($students_s1);
+            array_push($out,array_pop($students_s1));
         }
     }else{
         $substraction = $students_m2 - $students_m1;
         for ($i=0; $i < $substraction; $i++) { 
-            array_pop($students_s2);
+            array_push($out,array_pop($students_s2));
         }
     }
-    
     echo nl2br("\r\n");
+    //Combinar ambos arrays en uno solo
     $all_students = array_merge($students_s1, $students_s2);
-    
+    //Eliminar carnets repetidos
+    $all_students = array_unique($all_students);
     $calculations = create_groups(count($all_students), $n);
     
     $groups = $calculations[0];
     $students_left_out = $calculations[1];
-    shuffle($students_s1);
-    
+
+    //Crear los grupos, primer for para la cantidad de grupos
     $added = [];
-    for ($i=0; $i < $groups; $i++) { 
+
+    for ($i=0; $i < $groups; $i++) {
         echo ("Grupo ".($i+1).": ");
+        $name = get_name($s1, $s2, $con);
+        insert_group($con, $name, "LF155643", $s1, $s2);
+        //Segundo for para los cupos de cada grupo
         for ($j=0; $j < $n; $j++) { 
             $idx = get_idx(count($all_students), $added);
+            echo $all_students[$idx];
             array_push($added, $idx);
-            echo $all_students[$idx]." ";
+            add_student($con, $all_students[$idx], $name);
         }
         echo nl2br("\r\n");
     }
-    
 }
 
 ?>
